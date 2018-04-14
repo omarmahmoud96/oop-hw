d110ce283c129d9578edf3366bb6ef9a2ccb3115
OMAR MAHMOUD ABO EL ATTA <omarmahmoud747@gmail.com>
2018-04-15 00:21:59 +0200

    vehicle

--- parking/src/parking/Ticket.java
+++ parking/src/parking/Ticket.java
@@ -1,11 +1,22 @@
 package parking;
 
+import parking.Vehicle;
 public class Ticket {
 
 	String start_time;
+	Vehicle vehicle ;
 
-	public Ticket() {
-		// TODO Auto-generated constructor stub
+	public String getStart_time() {
+		return start_time;
 	}
 
+	public void setStart_time(String start_time) {
+		this.start_time = start_time;
+	}
+
+	public Ticket(String startTime,Vehicle vehicle) {
+		// TODO Auto-generated constructor stub
+		start_time = startTime ;
+		this.vehicle = vehicle ;
+	}
 }
--- parking/src/parking/Vehicle.java
+++ parking/src/parking/Vehicle.java
@@ -1,9 +1,16 @@
 package parking;
 
 public class Vehicle {
+	
+	public enum genre {
+		compact ,
+		regular
+	}
+	genre type ;
 
-	public Vehicle() {
+	public Vehicle(genre t) {
 		// TODO Auto-generated constructor stub
+		type = t ;
 	}
 
 }
